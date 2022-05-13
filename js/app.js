e3177f6f2ee6feba6f96f3e6f9c11537e7445c7a
vasabi0102 <98116313+vasabi0102@users.noreply.github.com>
2022-05-13 21:09:21 +0300

    zxcv

---
+++ js/app.js
@@ -0,0 +1,31 @@
+(() => {
+  "use strict";
+  let e = !1;
+  setTimeout(() => {
+    if (e) {
+      let e = new Event("windowScroll");
+      window.addEventListener("scroll", function (t) {
+        document.dispatchEvent(e);
+      });
+    }
+  }, 0),
+    (window.FLS = !0),
+    (function (e) {
+      let t = new Image();
+      (t.onload = t.onerror =
+        function () {
+          e(2 == t.height);
+        }),
+        (t.src =
+          "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA");
+    })(function (e) {
+      let t = !0 === e ? "webp" : "no-webp";
+      document.documentElement.classList.add(t);
+    });
+  const t = document.querySelector(".menu__icon"),
+    n = document.querySelector(".menu__list");
+  t &&
+    t.addEventListener("click", function (e) {
+      t.classList.toggle("_active"), n.classList.toggle("_active");
+    });
+})();
